335c568490d6f1bb8b1b16513331cf8443c5826d
Yonathan-Calderon <calderonprog@gmail.com>
2022-07-08 19:59:04 -0300

    Codigo Inicial - CryptoCoin

---
+++ src/components/TableCoins.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+import CoinRow from "./CoinRow";
+
+const titles = ["#", "Coin", "Price", "Price Change", "24hs Volume"]
+
+const TableCoins = ({coins, search}) => {
+    const filterCoins = coins.filter((coin) => coin.name.toLowerCase().includes(search.toLowerCase()) | coin.symbol.toLowerCase().includes(search.toLowerCase()));
+    return (
+        <table className="table table-dark mt-4 table-hover">
+            <thead>
+                <tr>
+                    { titles.map((title, index) => (
+                        <td key={index}>{title}</td>
+                    ))
+                    
+                    }
+                </tr>
+            </thead>
+            <tbody>
+                {filterCoins.map((coin, index) => (
+                    <CoinRow coin={coin} key={index} index={index + 1}/>
+                ))}
+            </tbody>
+        </table>
+    )
+}
+
+export default TableCoins;
